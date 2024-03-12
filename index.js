b793e2a1039fe64ccd761b8675a9e58bd8a3e575
HABIBULLAH <149287500+md8-habibullah@users.noreply.github.com>
2024-03-13 00:32:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Bot is Running...","Bot is Up..."];
 
 
 let currentIndex = 0;
